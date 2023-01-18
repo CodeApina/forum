18e81a07f39ff6ba82e13d2dc69ca34b0a4e078e
Henri <henri.saren@gmail.com>
2023-01-18 01:41:02 +0000

    Changed sql

--- backend/sql.php
+++ backend/sql.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost:3306";
 $db_user = "root";
-$db_password = "root";
+$db_password = "PerkelePaska69";
 $db_db = "forum";
 $db_port = 3306;
 $conn = new mysqli($db_host, $db_user, $db_password, $db_db);
