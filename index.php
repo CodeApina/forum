639ea3ffb3b6ccdbcd5866a510928ac02d7b1aab
HENRI SARÉN <henri.saren@edu.sakky.fi>
2023-01-24 18:44:14 +0200

    Testing

--- index.php
+++ index.php
@@ -17,30 +17,30 @@ include 'navbar.php';?>
                     return response.json();
                 }).then(function(posts){
                     console.log(posts);
-                    posts.foreach(
-                        <div class="card mb-4 bg-secondary text-light">
-                            <div class="card-body justify-content-center">
-                                <div class="d-flex justify-content-start">
-                                    <p class="medium mb-0 ms-0"><h5><?$row["title"]?></h5></p>
-                                </div>
-                                <div class="d-flex justify-content-start">
-                                    <p class="medium mb-0 ms-6"><?$row["content"]?></p>
-                                </div>
-                                <div class="d-flex justify-content-between">
-                                    <p class="small mb-0 ms-0"><?$row["user"]?></p>
-                                </div>
-                                <div class="d-flex flex-row align-items-center">
-                                    <p class="small text-muted mb-0">Upvote?</p>
-                                    <i class="far fa-thumbs-up mx-2 fa-xs text-white" style="margin-top: -0.16rem;"></i>
-                                    <p class="small text-muted mb-0"><?$row["likes"]?></p>
-                                </div>
-                            </div>
-                        </div>
-                    )
+                    //posts.foreach(
+                    //    <div class="card mb-4 bg-secondary text-light">
+                    //        <div class="card-body justify-content-center">
+                    //            <div class="d-flex justify-content-start">
+                    //                <p class="medium mb-0 ms-0"><h5><?$row["title"]?></h5></p>
+                    //            </div>
+                    //            <div class="d-flex justify-content-start">
+                    //                <p class="medium mb-0 ms-6"><?$row["content"]?></p>
+                    //            </div>
+                    //            <div class="d-flex justify-content-between">
+                    //                <p class="small mb-0 ms-0"><?$row["user"]?></p>
+                    //            </div>
+                    //            <div class="d-flex flex-row align-items-center">
+                    //                <p class="small text-muted mb-0">Upvote?</p>
+                    //                <i class="far fa-thumbs-up mx-2 fa-xs text-white" style="margin-top: -0.16rem;"></i>
+                    //                <p class="small text-muted mb-0"><?$row["likes"]?></p>
+                    //            </div>
+                    //        </div>
+                    //    </div>
+                    //)
                 })
                 document.write()
             </script>
-            <div class="card mb-4 bg-secondary text-light">
+            <!-- <div class="card mb-4 bg-secondary text-light">
                 <div class="card-body justify-content-center"></div>
                     <div class="d-flex justify-content-start">
                         <p class="medium mb-0 ms-0"><h5><?$row["title"]?></h5></p>
@@ -57,7 +57,7 @@ include 'navbar.php';?>
                         <p class="small text-muted mb-0"><?$row["likes"]?></p>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </body>
 </html>
