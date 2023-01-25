446087fd2fe02d832254cd859fa7500d7d17ae1b
Henri Sarén <henri.saren@gmail.com>
2023-01-25 18:30:03 +0200

    IT'S ALIVE

--- index.php
+++ index.php
@@ -41,18 +41,18 @@ include 'navbar.php';?>
                         html += '<div class="card mb-4 bg-secondary text-light">' +
                                     '<div class="card-body justify-content-center">' +
                                         '<div class="d-flex justify-content-start">' +
-                                            '<p id="title_box" class="medium mb-0 ms-0"><h5>' + post["title"] + '</h5></p>' +
+                                            '<p id="title_box" class="medium mb-0 ms-0"><h5>' + post.title + '</h5></p>' +
                                         '</div>' + 
                                         '<div class="d-flex justify-content-start">' +
-                                            '<p id="content_box" class="medium mb-0 ms-6">' + post["content"] +'</p>' +
+                                            '<p id="content_box" class="medium mb-0 ms-6">' + post.content +'</p>' +
                                         '</div>' +
                                         '<div class="d-flex justify-content-between">' +
-                                            '<p id="user_box" class="small mb-0 ms-0">' + post["user"] + '</p>' +
+                                            '<p id="user_box" class="small mb-0 ms-0">' + post.user + '</p>' +
                                         '</div>' + 
                                         '<div class="d-flex flex-row align-items-center">' +
                                             '<p class="small text-muted mb-0">Upvote?</p>' +
                                             '<i class="far fa-thumbs-up mx-2 fa-xs text-white" style="margin-top: -0.16rem;"></i>' +
-                                            '<p id="likes_box" class="small text-muted mb-0">' + post["likes"] + '</p>' +
+                                            '<p id="likes_box" class="small text-muted mb-0">' + post.likes + '</p>' +
                                         '</div>' +
                                     '</div>' +
                                 '</div>'
